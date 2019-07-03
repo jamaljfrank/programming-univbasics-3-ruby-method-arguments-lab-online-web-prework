76d816e634bd0dd64664db0750ca0e125416e7c7
Jamal Frank <jamaljfrank@gmail.com>
2019-07-03 03:28:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, ruby)
-  puts "Hi, my name is #{name} and I am learning to program in #{ruby}."
+def introduction_with_language_optional(name)
+  puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
 
 def introduction_with_language_optional(name, language)
